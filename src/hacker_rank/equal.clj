a3b5a116a25f1c0cd81042a5c87e1699892b1a97
JACE <jacegem@gmail.com>
2023-11-16 19:17:33 +0900

    fix code

--- src/hacker_rank/equal.clj
+++ src/hacker_rank/equal.clj
@@ -3,19 +3,6 @@
    https://see-ro-e.tistory.com/249
    ")
 
-#_(def op-count
-    (memoize (fn [n]
-               (loop [cnt n
-                      acc 0]
-                 (condp = cnt
-                   0 (+ 0 acc)
-                   1 (+ 1 acc)
-                   2 (+ 1 acc)
-                   3 (+ 2 acc)
-                   4 (+ 2 acc)
-                   5 (+ 1 acc)
-                   (recur (- cnt 5) (inc acc)))))))
-
 (defn op-count [n]
   (loop [cnt n
          acc 0]
@@ -29,7 +16,6 @@
       (recur (- cnt 5) (inc acc)))))
 
 (defn equal [arr]
-  (let [min-val (apply min arr)]
-    (->> (map #(- % min-val) arr)
-         (map op-count)
-         (reduce +))))
+  (->> (map #(- % (apply min arr)) arr)
+       (map op-count)
+       (reduce +)))

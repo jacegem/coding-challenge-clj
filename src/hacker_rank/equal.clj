c7dc74437627e79251863eddb708d2abf60398b4
JACE <jacegem@gmail.com>
2023-11-16 20:40:54 +0900

    fix link

--- src/hacker_rank/equal.clj
+++ src/hacker_rank/equal.clj
@@ -1,21 +1,25 @@
 (ns hacker-rank.equal
   "https://www.hackerrank.com/challenges/equal/problem
-   https://see-ro-e.tistory.com/249
+   https://jacegem.github.io/blog/2023/2023-11-16-clojure-hacker-rank-equal/
    ")
+
+(def count-map {0 0
+                1 1
+                2 1
+                3 2
+                4 2
+                5 1})
 
 (defn op-count [n]
   (loop [cnt n
          acc 0]
-    (condp = cnt
-      0 (+ 0 acc)
-      1 (+ 1 acc)
-      2 (+ 1 acc)
-      3 (+ 2 acc)
-      4 (+ 2 acc)
-      5 (+ 1 acc)
+    (if-let [val (count-map cnt)]
+      (+ acc val)
       (recur (- cnt 5) (inc acc)))))
 
 (defn equal [arr]
   (->> (map #(- % (apply min arr)) arr)
        (map op-count)
        (reduce +)))
+
+(equal [2 2 3 7])
